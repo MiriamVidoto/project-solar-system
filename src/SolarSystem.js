1a5c1f090b3782ae8783f1941e4b7a886495557e
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-06-21 16:14:49 -0300

    Requisito 2 ok 🚀

---
+++ src/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <p> Lista dos planetas</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
