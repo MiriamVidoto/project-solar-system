6f4492145a015d1d316e043509ed2c01c7351cfa
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-06-21 18:47:03 -0300

    Requisito 4 ok 🚀

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+export default PlanetCard;
