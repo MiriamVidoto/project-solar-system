ada9b83e0b214c6f4584373a49fc494100445349
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-06-22 12:43:15 -0300

    Correção do lint 🚀

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -6,16 +6,16 @@ class MissionCard extends React.Component {
     const { name, year, country, destination } = this.props;
     return (
       <div data-testid="mission-card">
-        <p data-testid="mission-name"> {name } </p>
-        <p data-testid="mission-year"> {year} </p>
-        <p data-testid="mission-country"> {country} </p>
-        <p data-testid="mission-destination"> {destination} </p>
+        <p data-testid="mission-name">{name}</p>
+        <p data-testid="mission-year">{year}</p>
+        <p data-testid="mission-country">{country}</p>
+        <p data-testid="mission-destination">{destination}</p>
       </div>
     );
   }
 }
 
-MissionCard.protoTypes = {
+MissionCard.propTypes = {
   name: PropTypes.string.isRequired,
   year: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
